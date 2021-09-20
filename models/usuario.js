d0da5f68d4c16682e3667673b88171ee36e58923
usuarioPPSE <mailPPSE.ppse.es>
2021-09-20 08:56:46 +0200

    cambiios

--- models/usuario.js
+++ models/usuario.js
@@ -1,9 +1,29 @@
- class Usuario {
-    constructor(nombre,clave){
+const crypto = require("crypto");
+class Usuario {
+
+
+    constructor(nombre) {
+        this.nombre = nombre;
+        this.clave = "";
+        this.sal = crypto.randomBytes(6).toString('hex');
+    }
+    get Nombre(){
+        return this.nombre;
+    }
+    get Clave(){
+        return this.clave;
+    }
+    set Nombre(nombre){
         this.nombre=nombre;
-        this.clave=clave;
-       
+    }
+    set Clave(clave){
+        this.clave = crypto.createHash("sha256").update(clave + this.sal).digest("hex")
     }
   
-} 
+    validarUsuario(otro_usuario, otra_clave) {
+      
+        return (otro_usuario == this.nombre && crypto.createHash("sha256").update(otra_clave + this.sal).digest("hex") == this.clave)
+    }
+
+}
 module.exports = Usuario;
--- routes/piscinaPPSE.js
+++ routes/piscinaPPSE.js
@@ -4,48 +4,108 @@ const modeloPiscina = require("../models/piscina")
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const { check, validationResult, body } = require('express-validator');
+const Usuario = require('../models/usuario');
 
-var usuario = "paco"
-var clave = "falsa"
-var tokenfalso = "mentira"
+var regEx_sanityze = /<script>|<\/script>|<iframe>|<\/iframe>|javascript:|onload=|alert(1)|<|>|\"1=1\"|\'1\'=\'1\'|--,1=|SELECT/i
+var privadaJWT = "Mq@45oP!2"
 /**
  * se utiliza un array de piscinas para almacenar los recusos
  * no es el objetivo del módulo el trabajar con base de datos
  */
 var piscinas = new Array();
+//se crea la variable usuario, en otros casos se almacenaría en la base de datos.
+var usuario = new Usuario();
+usuario.nombre = "Pedro";
+usuario.Clave="1234"
 var contador = 0;
 
-function init(){
-  let tempopiscina=  new modeloPiscina.Piscina("Torre Stark", "Iron Man", new Date().toISOString().slice(0, 10))
-  piscinas.push(tempopiscina);
-  contador++;
-  tempopiscina.Id = contador;
-  
-  tempopiscina= new modeloPiscina.Piscina("C/Titan", "Thanos", new Date().toISOString().slice(0, 10))
+function init() {
+  let tempopiscina = new modeloPiscina.Piscina("Torre Stark", "Iron Man", new Date().toISOString().slice(0, 10))
   piscinas.push(tempopiscina);
   contador++;
   tempopiscina.Id = contador;
 
-  tempopiscina=new modeloPiscina.Piscina("Avda Dimensión Espejo", "Doctor Strange", new Date().toISOString().slice(0, 10))
+  tempopiscina = new modeloPiscina.Piscina("C/Titan", "Thanos", new Date().toISOString().slice(0, 10))
   piscinas.push(tempopiscina);
   contador++;
   tempopiscina.Id = contador;
-  }
+
+  tempopiscina = new modeloPiscina.Piscina("Avda Dimensión Espejo", "Doctor Strange", new Date().toISOString().slice(0, 10))
+  piscinas.push(tempopiscina);
+  contador++;
+  tempopiscina.Id = contador;
+}
 /**
  * Añadir el código necesario para validar los campos de entrada y generar 
  * un token
  */
 router.post("/login",
+  check('usuario').exists().withMessage('Necesario el usuario').customSanitizer((value) => {
+    if (value != undefined)
+      value = value.replace(regEx_sanityze, "")
+    return value;
+  }),
+  check('clave').exists().withMessage('Necesaria la clave').customSanitizer((value) => {
+    if (value != undefined) {
+      value = value.replace(regEx_sanityze, "")
+    }
+    return value;
+  }),
   (req, res) => {
-    return res.json(tokenfalso);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  
+    if (usuario.validarUsuario(req.body.usuario,req.body.clave)){ // = usuario.nombre && req.body.clave == usuario.clave) {
+      var token = jwt.sign({ usuario: usuario.nombre }, privadaJWT, { algorithm: 'HS512', expiresIn: 86400 });
+      return res.json(token);
+    } else {
+      return res.json({ errors: [{ msg: "usuario o clave incorrectos", param: "todos", location: "body" }] })
+    }
+
   })
+
+function recursoprotegido(req, res, next) {
+  if (!req.headers.authorization) {
+    return res.status(401).json({ errors: [{ msg: "Petición sin token" }] });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, privadaJWT, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ errors: [{ msg: "token incorrecto", param: "token", location: "body" }] })
+    } else {
+      //se almacena en la petición para su uso en el método
+      req.decoded = decoded;
+      next();
+    }
+  });
+}
 
 /**
  * Alta de una nueva piscina, validar que se tiene permisos (enviar el token), comprobar los parámetros y desinfectar los mismos
  */
 router.post(
   '/piscina',
+  recursoprotegido,
+  check('direccion').exists().withMessage('Campo dirección necesario').isLength({ min: 4, max: 150 }).withMessage('Longitud de la dirección entre 4 y 150').customSanitizer((value) => {
+    if (value != undefined)
+      value = value.replace(regEx_sanityze, "")
+    return value;
+  }),
+  check('propietario').exists().withMessage('Campo propietario necesario').isLength({ min: 10, max: 110 }).withMessage('Longitud de propietario debe estar entre 10 y 110').customSanitizer((value) => {
+    if(value != undefined)
+    value = value.replace(regEx_sanityze, "")
+    return value;
+  }),
   (req, res) => {
+
+      //errores de entrada
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
     var piscina = new modeloPiscina.Piscina(req.body.direccion, req.body.propietario, new Date().toISOString().slice(0, 10))
     piscinas.push(piscina);
     contador++;
@@ -60,15 +120,28 @@ router.post(
  */
 router.delete(
   '/piscina/:id',
+  recursoprotegido,
+  check('id').exists().withMessage('Necesita el identificador de la piscina').isInt().withMessage('El id ha de ser un entero').customSanitizer((value) => {
+    value = value.replace(regEx_sanityze, "")
+    return value;
+  }),
   (req, res) => {
+
     var length_antes = piscinas.length, length_despues;
     var tempo;
+
+  //errores de entrada
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
     tempo = piscinas.filter(element => {
       return element.Id != parseInt(req.params.id)
     });
     //eb caso de no devolver nada
-    if(tempo!=undefined){
-      piscinas=tempo
+    if (tempo != undefined) {
+      piscinas = tempo
     }
     length_despues = piscinas.length;
     res.json(length_antes - length_despues)
@@ -77,20 +150,32 @@ router.delete(
 /**
  *  Datos de todas las piscinas, comprobar si se tiene permisos. 
  * */
-router.get('/piscina', function (req, res, next) {
-  res.json(piscinas);
-});
+router.get('/piscina',
+  recursoprotegido,
+  function (req, res, next) {
+    res.json(piscinas);
+  });
 /**
  *  Datos de una piscina concreta, Comprobar si se tiene permisos y el parámetro es correcto. 
  * */
-router.get('/piscina/:id', function (req, res, next) {
-  var piscina = piscinas.find(element => element.Id == parseInt(req.params.id));
-  if (piscina == undefined) {
-    res.status(404).json({ error: "No existe la piscina" })
-  } else {
-    res.json(piscina);
-  }
-});
+router.get('/piscina/:id',
+  recursoprotegido,
+  check('id').exists().withMessage('Necesita el identificador de la piscina').isInt().withMessage('El id ha de ser un entero'),
+  function (req, res, next) {
+    
+    //errores de entrada
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    var piscina = piscinas.find(element => element.Id == parseInt(req.params.id));
+    if (piscina == undefined) {
+      res.status(404).json({ error: "No existe la piscina" })
+    } else {
+      res.json(piscina);
+    }
+  });
 
 
 /**
@@ -98,7 +183,22 @@ router.get('/piscina/:id', function (req, res, next) {
  */
 router.post(
   '/piscina/:id/medicion',
+  recursoprotegido,
+  check('id').exists().withMessage('Necesita el identificador de la piscina').isInt().withMessage('El id ha de ser un entero'),
+  check('ph').exists().withMessage('Se necesita el nivel de ph').isInt().withMessage('El PH ha de ser un entero'),
+  check('cloro').exists().withMessage('Se necesita el nivel de cloro').isInt().withMessage('El cloro ha de ser un entero'),
+  check('gramos_ph').exists().withMessage('Se necesita los gramos de pho').isInt().withMessage('Los gramos de ph han de ser un entero'),
+  check('gramos_cloro').exists().withMessage('Se necesita los gramos de cloro').isInt().withMessage('Los gramos de cloro han de ser un entero'),
+  check('antialgas').exists().withMessage('Se necesita los gramos de antialgas').isInt().withMessage('Los gramos de antialgas han de ser un entero'),
+  check('fluoculante').exists().withMessage('Se necesita los gramos de fluoculante').isInt().withMessage('Los gramos de fluoculante han de ser un entero'),
   (req, res) => {
+
+    //errores de entrada
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     var piscina = piscinas.find(element => element.Id == parseInt(req.params.id));
     var medicion;
     if (piscina == undefined) {
@@ -108,8 +208,6 @@ router.post(
       piscina.addMedicion(medicion)
       res.json(medicion);
     }
-
-
   }
 );
 /**
@@ -117,9 +215,19 @@ router.post(
 */
 router.delete(
   '/piscina/:idpiscina/medicion/:id',
+  recursoprotegido,
+  check('idpiscina').exists().withMessage('Necesita el identificador de la piscina').isInt().withMessage('El id de la pisicna  ha de ser un entero'),
+  check('id').exists().withMessage('Necesita el identificador de la medición').isInt().withMessage('El id ha de ser un entero'),
   (req, res) => {
     var length_antes, length_despues;
     var tempo;
+
+    //errores de entrada
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     var piscina = piscinas.find(element => element.Id == parseInt(req.params.idpiscina));
     var medicion;
     if (piscina == undefined) {
@@ -130,8 +238,8 @@ router.delete(
         return element.Id != parseInt(req.params.id)
       });
       //en caso de no devolver nada
-      if(tempo!=undefined){
-        piscina.Mediciones=tempo
+      if (tempo != undefined) {
+        piscina.Mediciones = tempo
       }
       length_despues = piscina.Mediciones.length;
       res.json(length_antes - length_despues)
@@ -142,19 +250,29 @@ router.delete(
 /**
 *  Obtener una medición concreta. 
 * */
-router.get('/piscina/:idpiscina/medicion/:id', function (req, res, next) {
-  var piscina = piscinas.find(element => element.Id == parseInt(req.params.idpiscina));
-  var medicion;
-  if (piscina == undefined) {
-    res.status(404).json({ error: "No existe la piscina" })
-  } else {
-    medicion = piscina.Mediciones.find(element => element.Id == parseInt(req.params.id))
-    if (medicion == undefined) {
-      res.status(404).json({ error: "No existe la medicion" })
-    } else {
-      res.json(medicion)
+router.get('/piscina/:idpiscina/medicion/:id',
+  recursoprotegido,
+  check('idpiscina').exists().withMessage('Necesita el identificador de la piscina').isInt().withMessage('El id de la pisicna  ha de ser un entero'),
+  check('id').exists().withMessage('Necesita el identificador de la medición').isInt().withMessage('El id ha de ser un entero'),
+  function (req, res, next) {
+    //errores de entrada
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  }
-});
 
-module.exports = {router, init};
+    var piscina = piscinas.find(element => element.Id == parseInt(req.params.idpiscina));
+    var medicion;
+    if (piscina == undefined) {
+      res.status(404).json({ error: "No existe la piscina" })
+    } else {
+      medicion = piscina.Mediciones.find(element => element.Id == parseInt(req.params.id))
+      if (medicion == undefined) {
+        res.status(404).json({ error: "No existe la medicion" })
+      } else {
+        res.json(medicion)
+      }
+    }
+  });
+
+module.exports = { router, init };
